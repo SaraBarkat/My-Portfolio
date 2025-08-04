fa6ff733a669a35e51e816e717c1662f95fa73ee
SaraBarkat <ms_barkat@esi.dz>
2025-08-04 17:49:07 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-let menuIcon=document.querySelector('.menu-icon');
-let navbar=document.querySelector('.navbar');
-menuIcon.Onclick=()=>{
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}   
+// MENU TOGGLE
+let menuIcon = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+document.getElementById('contact-form').addEventListener('submit', async function (e) {
+  e.preventDefault();
+
+  const formData = {
+    name: this.name.value,
+    email: this.email.value,
+    subject: this.subject.value,
+    message: this.message.value,
+  };
+
+  const res = await fetch('http://localhost:8000/send-email/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(formData),
+  });
+
+  const data = await res.json();
+  alert(data.message);
+});
