c6d8a345a0754537197a334a3ce7f8d2e7bd5a58
SaraBarkat <ms_barkat@esi.dz>
2025-08-04 18:32:21 +0100

    Final update

--- script.js
+++ script.js
@@ -6,22 +6,3 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
 };
-document.getElementById('contact-form').addEventListener('submit', async function (e) {
-  e.preventDefault();
-
-  const formData = {
-    name: this.name.value,
-    email: this.email.value,
-    subject: this.subject.value,
-    message: this.message.value,
-  };
-
-  const res = await fetch('http://localhost:8000/send-email/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(formData),
-  });
-
-  const data = await res.json();
-  alert(data.message);
-});
